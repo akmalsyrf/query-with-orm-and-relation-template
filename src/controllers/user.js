f548430833e3c6fb4ce17093df1c5caefe8f88bc
akmalsyrf <dev.akmal69@gmail.com>
2021-12-22 09:35:07 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,16 @@
 // import model here
+const { user } = require("../../models");
 
 exports.addUsers = async (req, res) => {
-    // code here
-}
+  // code here
+  try {
+    const data = req.body;
+    const createdData = await user.create(data);
+    res.send({ status: "success", data: createdData });
+  } catch (error) {
+    res.send({
+      status: "failed",
+      message: "server error",
+    });
+  }
+};
